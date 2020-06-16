e7fd55875a53f4500b8382a6d6f3cc1fc264560a
emgao <do.you.canoe@gmail.com>
2020-06-16 09:13:25 -0400

    Updates

---
+++ precache-manifest.7eddaf535e8e4bd29c30bc9e11e57615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec80689af9a431a92c8dfb14fd8fb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc72ae4830c1dba8332",
+    "url": "/static/css/main.68ed3f2b.chunk.css"
+  },
+  {
+    "revision": "26cd26c868df4b158cfc",
+    "url": "/static/js/2.b66d5295.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b66d5295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc72ae4830c1dba8332",
+    "url": "/static/js/main.04ca8161.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
